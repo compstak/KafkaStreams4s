da3c264ca0302fc48a5e1e38f232c4a5aafc2c7a
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2020-07-15 19:30:03 -0400

    Fix typo

--- debezium/src/main/scala/compstak/kafkastreams4s/debezium/DebeziumCompositeType.scala
+++ debezium/src/main/scala/compstak/kafkastreams4s/debezium/DebeziumCompositeType.scala
@@ -24,7 +24,7 @@ object DebeziumCompositeType {
   /**
    * Returns a simple debezium "struct" schema with a single optional field
    */
-  def fromPromitiveOption[A: DebeziumPrimitiveType](fieldName: String): DebeziumCompositeType[A] =
+  def fromPrimitiveOption[A: DebeziumPrimitiveType](fieldName: String): DebeziumCompositeType[A] =
     new DebeziumCompositeType[A] {
       def schema: List[DebeziumFieldSchema] =
         List(DebeziumFieldSchema(DebeziumPrimitiveType[A].debeziumType, true, fieldName))
