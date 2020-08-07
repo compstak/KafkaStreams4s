9e61e5f5e30f5a0dd1ab441d14519a393352798a
Zach McCoy <zach.mccoy@banno.com>
2020-08-07 18:47:27 -0500

    Add vulcan to sbt aggregate, formatting

--- vulcan/src/main/scala/compstak/kafkastreams4s/vulcan/VulcanSerdes.scala
+++ vulcan/src/main/scala/compstak/kafkastreams4s/vulcan/VulcanSerdes.scala
@@ -7,36 +7,34 @@ import org.apache.kafka.streams.state.KeyValueStore
 import vulcan.{Codec => VCodec}
 
 object VulcanSerdes {
-  def serdeForVulcan[A : VCodec]: Serde[A] = new VulcanSerdeCreation[A]
+  def serdeForVulcan[A: VCodec]: Serde[A] = new VulcanSerdeCreation[A]
 
-  def producedForVulcan[K : VCodec, V : VCodec]
-  : Produced[K, V] =
+  def producedForVulcan[K: VCodec, V: VCodec]: Produced[K, V] =
     Produced.`with`(serdeForVulcan[K], serdeForVulcan[V])
 
-  def materializedForVulcan[K : VCodec, V : VCodec]
-  : Materialized[K, V, KeyValueStore[Bytes, Array[Byte]]] =
+  def materializedForVulcan[K: VCodec, V: VCodec]: Materialized[K, V, KeyValueStore[Bytes, Array[Byte]]] =
     Materialized
       .`with`(serdeForVulcan[K], serdeForVulcan[V])
 
-  def consumedForVulcan[K : VCodec, V : VCodec]
-  : Consumed[K, V] =
+  def consumedForVulcan[K: VCodec, V: VCodec]: Consumed[K, V] =
     Consumed.`with`(serdeForVulcan[K], serdeForVulcan[V])
 
-  def groupedForVulcan[K : VCodec, V : VCodec]: Grouped[K, V] =
+  def groupedForVulcan[K: VCodec, V: VCodec]: Grouped[K, V] =
     Grouped.`with`(serdeForVulcan[K], serdeForVulcan[V])
 
 }
 
 class VulcanSerdeCreation[T: VCodec] extends Serde[T] {
-  override def deserializer(): Deserializer[T] = new Deserializer[T] {
-    override def deserialize(topic: String, data: Array[Byte]): T = {
-      val codec = implicitly[VCodec[T]]
-      codec.schema.flatMap(schema => codec.decode(data, schema)).fold(error => throw error.throwable, identity)
+  override def deserializer(): Deserializer[T] =
+    new Deserializer[T] {
+      override def deserialize(topic: String, data: Array[Byte]): T = {
+        val codec = implicitly[VCodec[T]]
+        codec.schema.flatMap(schema => codec.decode(data, schema)).fold(error => throw error.throwable, identity)
+      }
     }
-  }
-  override def serializer(): Serializer[T] = new Serializer[T] {
-    override def serialize(topic: String, data: T): Array[Byte] = {
-      VCodec.toBinary(data).fold(error =>  throw error.throwable, identity)
+  override def serializer(): Serializer[T] =
+    new Serializer[T] {
+      override def serialize(topic: String, data: T): Array[Byte] =
+        VCodec.toBinary(data).fold(error => throw error.throwable, identity)
     }
-  }
 }
