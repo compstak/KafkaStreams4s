f2434d87fc6dc6759dc8b34c4fb8e8b21485c0e3
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2020-09-09 12:36:55 -0400

    Fix compilation and split test runners into 2 versions

--- testing/src/main/scala/compstak/kafkastreams4s/testing/KafkaStreamsTestRunner.scala
+++ testing/src/main/scala/compstak/kafkastreams4s/testing/KafkaStreamsTestRunner.scala
@@ -24,7 +24,9 @@ import scala.collection.JavaConverters._
 import compstak.kafkastreams4s._
 import org.apache.kafka.streams.TestOutputTopic
 
-class KafkaStreamsTestRunner[F[_]: Sync, HK[_]: Codec, HV[_]: Codec, KA: HK, A: HV, KB: HK, B: HV](
+private[kafkastreams4s] class KafkaStreamsTestRunner[F[_]: Sync, HK[_]: Codec, HV[
+  _
+]: Codec, KA: HK, A: HV, KB: HK, B: HV](
   f: STable[HK, KA, HV, A] => STable[HK, KB, HV, B]
 ) {
 
@@ -38,7 +40,7 @@ class KafkaStreamsTestRunner[F[_]: Sync, HK[_]: Codec, HV[_]: Codec, KA: HK, A: 
       topicOut <- randomString
       topo <- topology(topicIn, topicOut)
       bs <- testDriverResource[F](topo).use(driver =>
-        inputTestTable[F, HK, HV](driver, topicIn, input: _*) >> outputTestTable[F, HK, KB, HV, B](driver, topicOut)
+        inputTestTable2[F, HK, HV](driver, topicIn, input: _*) >> outputTestTable2[F, HK, KB, HV, B](driver, topicOut)
       )
     } yield bs
 
@@ -48,8 +50,8 @@ class KafkaStreamsTestRunner[F[_]: Sync, HK[_]: Codec, HV[_]: Codec, KA: HK, A: 
       topicOut <- randomString
       topo <- topology(topicIn, topicOut)
       bs <- testDriverResource[F](topo).use(driver =>
-        inputTestTable[F, HK, HV](driver, topicIn, input.toList.tupleLeft(ev("key")): _*) >>
-          outputTestTableList[F, HK, KB, HV, B](driver, topicOut)
+        inputTestTable2[F, HK, HV](driver, topicIn, input.toList.tupleLeft(ev("key")): _*) >>
+          outputTestTableList2[F, HK, KB, HV, B](driver, topicOut)
       )
     } yield bs
 
@@ -65,7 +67,9 @@ object KafkaStreamsTestRunner {
   def testDriverResource[F[_]: Sync](topo: Topology): Resource[F, TopologyTestDriver] =
     Resource.make(props.map(p => new TopologyTestDriver(topo, p)))(d => Sync[F].delay(d.close))
 
-  def inputTestTable[F[_], HK[_], HV[_]]: InputPartiallyAppliedF[F, HK, HV] = new InputPartiallyAppliedF[F, HK, HV]
+  def inputTestTable2[F[_], HK[_], HV[_]]: InputPartiallyAppliedF[F, HK, HV] = new InputPartiallyAppliedF[F, HK, HV]
+
+  def inputTestTable[F[_], C[_]]: InputPartiallyAppliedF[F, C, C] = new InputPartiallyAppliedF[F, C, C]
 
   private[kafkastreams4s] class InputPartiallyAppliedF[F[_], HK[_], HV[_]](private val dummy: Unit = ())
       extends AnyVal {
@@ -79,7 +83,7 @@ object KafkaStreamsTestRunner {
     }
   }
 
-  def outputTestTable[F[_]: Sync, HK[_]: Codec, K: HK, HV[_]: Codec, V: HV](
+  def outputTestTable2[F[_]: Sync, HK[_]: Codec, K: HK, HV[_]: Codec, V: HV](
     driver: TopologyTestDriver,
     name: String
   ): F[Map[K, V]] = {
@@ -87,11 +91,27 @@ object KafkaStreamsTestRunner {
     Sync[F].delay(out.readKeyValuesToMap.asScala.toMap)
   }
 
-  def outputTestTableList[F[_]: Sync, HK[_]: Codec, K: HK, HV[_]: Codec, V: HV](
+  def outputTestTable[F[_]: Sync, C[_]: Codec, K: C, V: C](
+    driver: TopologyTestDriver,
+    name: String
+  ): F[Map[K, V]] = {
+    val out = driver.createOutputTopic(name, Codec[C].serde[K].deserializer, Codec[C].serde[V].deserializer)
+    Sync[F].delay(out.readKeyValuesToMap.asScala.toMap)
+  }
+
+  def outputTestTableList2[F[_]: Sync, HK[_]: Codec, K: HK, HV[_]: Codec, V: HV](
     driver: TopologyTestDriver,
     name: String
   ): F[List[V]] = {
     val out = driver.createOutputTopic(name, Codec[HK].serde[K].deserializer, Codec[HV].serde[V].deserializer)
+    Sync[F].delay(out.readValuesToList.asScala.toList)
+  }
+
+  def outputTestTableList2[F[_]: Sync, C[_]: Codec, K: C, V: C](
+    driver: TopologyTestDriver,
+    name: String
+  ): F[List[V]] = {
+    val out = driver.createOutputTopic(name, Codec[C].serde[K].deserializer, Codec[C].serde[V].deserializer)
     Sync[F].delay(out.readValuesToList.asScala.toList)
   }
 
@@ -103,11 +123,17 @@ object KafkaStreamsTestRunner {
       p
     }
 
-  def run[F[_]: Sync, HK[_]: Codec, HV[_]: Codec, KA: HK, A: HV, KB: HK, B: HV](
+  def run2[F[_]: Sync, HK[_]: Codec, HV[_]: Codec, KA: HK, A: HV, KB: HK, B: HV](
     f: STable[HK, KA, HV, A] => STable[HK, KB, HV, B],
     input: (KA, A)*
   ): F[Map[KB, B]] =
     new KafkaStreamsTestRunner[F, HK, HV, KA, A, KB, B](f).run(input: _*)
+
+  def run[F[_]: Sync, C[_]: Codec, KA: C, A: C, KB: C, B: C](
+    f: STable[C, KA, C, A] => STable[C, KB, C, B],
+    input: (KA, A)*
+  ): F[Map[KB, B]] =
+    new KafkaStreamsTestRunner[F, C, C, KA, A, KB, B](f).run(input: _*)
 
   def runList[F[_]: Sync, C[_]: Codec, A: C, B: C](
     f: STable[C, String, C, A] => STable[C, String, C, B],
