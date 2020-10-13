390972f7617f496e9f29caf142ec26468f7dbba8
Paul Snively <types.snively@gmail.com>
2020-10-13 17:33:49 -0400

    Added System.exit(1) to failure case in Platform#runStreams.

--- core/src/main/scala/compstak/kafkastreams4s/Platform.scala
+++ core/src/main/scala/compstak/kafkastreams4s/Platform.scala
@@ -1,6 +1,6 @@
 package compstak.kafkastreams4s
 
-import cats.effect.{Async, Resource, Sync}
+import cats.effect.{Async, ExitCase, ExitCode, Resource, Sync}
 import org.apache.kafka.streams.{KafkaStreams, Topology}
 import org.apache.kafka.streams.KafkaStreams.State
 import java.util.Properties
@@ -8,23 +8,34 @@ import java.time.Duration
 
 object Platform {
   def streamsResource[F[_]: Sync](top: Topology, props: Properties, timeout: Duration): Resource[F, KafkaStreams] =
-    Resource.make(Sync[F].delay(new KafkaStreams(top, props)))(s => Sync[F].delay(s.close(timeout)))
+    Resource.make(Sync[F].delay(new KafkaStreams(top, props)))(s =>
+      Sync[F].delay {
+        s.close(timeout)
+      }
+    )
 
-  def runStreams[F[_]: Async](streams: KafkaStreams): F[Unit] =
-    Async[F].async { (k: Either[Throwable, Unit] => Unit) =>
+  def runStreams[F[_]: Async](streams: KafkaStreams): F[Unit] = {
+    val run = Async[F].async { (k: Either[Throwable, Unit] => Unit) =>
       streams.setUncaughtExceptionHandler { (_: Thread, e: Throwable) =>
         k(Left(e))
       }
 
       streams.setStateListener { (state: State, _: State) =>
         state match {
-          case State.ERROR => k(Left(new RuntimeException("The KafkaStreams went into an ERROR state.")))
+          case State.ERROR =>
+            k(Left(new RuntimeException("The KafkaStreams went into an ERROR state.")))
           case _ => ()
         }
       }
 
       streams.start()
     }
+
+    Async[F].guaranteeCase(run) {
+      case ExitCase.Completed => Async[F].pure(())
+      case _ => Async[F].delay(System.exit(1))
+    }
+  }
 
   def run[F[_]: Async](topo: Topology, props: Properties, timeout: Duration): F[Unit] =
     streamsResource[F](topo, props, timeout).use(runStreams[F])
