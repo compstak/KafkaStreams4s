c5e658a7de9c85395f7a6dded82bd651de8d36c0
Paul Snively <types.snively@gmail.com>
2020-10-15 10:50:21 -0400

    Revised to represent complete or incomplete shutdown rather than hardcode a JVM exit upon incomplete shutdown.

--- core/src/main/scala/compstak/kafkastreams4s/Platform.scala
+++ core/src/main/scala/compstak/kafkastreams4s/Platform.scala
@@ -1,42 +1,53 @@
 package compstak.kafkastreams4s
 
-import cats.effect.{Async, ExitCase, ExitCode, Resource, Sync}
+import cats._, cats.implicits._
+import cats.effect.{Async, Concurrent, ExitCase, ExitCode, Resource, Sync}
+import cats.effect.implicits._
+import cats.effect.concurrent.Deferred
 import org.apache.kafka.streams.{KafkaStreams, Topology}
 import org.apache.kafka.streams.KafkaStreams.State
 import java.util.Properties
 import java.time.Duration
+import org.apache.kafka.common.protocol.types.Field.Bool
+
+sealed trait ShutdownStatus
+final case object ShutdownComplete extends ShutdownStatus
+final case object ShutdownIncomplete extends ShutdownStatus
+
+object ShutdownStatus {
+  def apply(shutdown: Boolean): ShutdownStatus =
+    if (shutdown) {
+      ShutdownComplete
+    } else {
+      ShutdownIncomplete
+    }
+}
 
 object Platform {
-  def streamsResource[F[_]: Sync](top: Topology, props: Properties, timeout: Duration): Resource[F, KafkaStreams] =
-    Resource.make(Sync[F].delay(new KafkaStreams(top, props)))(s =>
-      Sync[F].delay {
-        s.close(timeout)
-      }
-    )
+  def run[F[_]: Concurrent](top: Topology, props: Properties, timeout: Duration): F[ShutdownStatus] =
+    for {
+      d <- Deferred[F, Boolean]
+      r <- Resource
+        .make(
+          Sync[F].delay(new KafkaStreams(top, props))
+        )(s => Sync[F].delay(s.close(timeout)).flatMap(b => d.complete(b)))
+        .use { streams =>
+          Async[F].async { (k: Either[Throwable, Unit] => Unit) =>
+            streams.setUncaughtExceptionHandler { (_: Thread, e: Throwable) =>
+              k(Left(e))
+            }
 
-  def runStreams[F[_]: Async](streams: KafkaStreams): F[Unit] = {
-    val run = Async[F].async { (k: Either[Throwable, Unit] => Unit) =>
-      streams.setUncaughtExceptionHandler { (_: Thread, e: Throwable) =>
-        k(Left(e))
-      }
+            streams.setStateListener { (state: State, _: State) =>
+              state match {
+                case State.ERROR =>
+                  k(Left(new RuntimeException("The KafkaStreams went into an ERROR state.")))
+                case _ => ()
+              }
+            }
 
-      streams.setStateListener { (state: State, _: State) =>
-        state match {
-          case State.ERROR =>
-            k(Left(new RuntimeException("The KafkaStreams went into an ERROR state.")))
-          case _ => ()
+            streams.start()
+          }
         }
-      }
-
-      streams.start()
-    }
-
-    Async[F].guaranteeCase(run) {
-      case ExitCase.Completed => Async[F].pure(())
-      case _ => Async[F].delay(System.exit(1))
-    }
-  }
-
-  def run[F[_]: Async](topo: Topology, props: Properties, timeout: Duration): F[Unit] =
-    streamsResource[F](topo, props, timeout).use(runStreams[F])
+        .redeemWith(_ => d.get, _ => d.get)
+    } yield ShutdownStatus(r)
 }
